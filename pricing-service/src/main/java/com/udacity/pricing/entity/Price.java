ec21f2858697aa27449d88f516280b65cd5e926d
Euan Ramsay <euancramsay@aol.com>
2021-01-04 15:30:24 +0000

    Covert to microservice on Eureka.

--- pricing-service/src/main/java/com/udacity/pricing/entity/Price.java
+++ pricing-service/src/main/java/com/udacity/pricing/entity/Price.java
@@ -1,11 +1,21 @@
-package com.udacity.pricing.domain.price;
+package com.udacity.pricing.entity;
 
+import javax.persistence.Id;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import java.math.BigDecimal;
 
 /**
  * Represents the price of a given vehicle, including currency.
  */
+@Entity
 public class Price {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
     private String currency;
     private BigDecimal price;
@@ -18,6 +28,14 @@ public class Price {
         this.currency = currency;
         this.price = price;
         this.vehicleId = vehicleId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getCurrency() {
